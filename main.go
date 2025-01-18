5f7230cc0cc3fcd979b89cb0981d6c9bfecb9750
filipegms5 <filipegms5@gmail.com>
2025-01-18 12:45:24 -0300

    Correção port

--- main.go
+++ main.go
@@ -9,8 +9,12 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8000" // Default port if not specified
+		port = ":8000" // Default port if not specified
 	}
 	router := router.SetupRouter()
-	router.Run(os.Getenv(port))
+	err := router.Run(port)
+	if err != nil {
+		panic("[Error] failed to start Gin server due to: " + err.Error())
+		return
+	}
 }
